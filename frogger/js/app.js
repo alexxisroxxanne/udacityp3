b54fe5e9e6178d715eabd76f4c9071e4e9375cc7
Alexxis Johnson <alexxis.r.p.johnson@gmail.com>
2015-08-07 15:30:21 -0700

    Re-init repository due to push errors

--- frogger/js/app.js
+++ frogger/js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -8,13 +8,11 @@ var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
 
     // Set the enemy initial location
-    var enemyObj = {loc: loc,
-                speed: speed};
-
-    enemyObj.loc = (101,83);
+    this.x = x;
+    this.y = y;
 
     // Set the enemy speed
-    enemyObj.speed = enemyObj.loc - dt;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -23,12 +21,17 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    var loc = Enemy.loc;
-    loc * dt;
+    this.x = this.x + this.speed * dt;
 
     // Update the enemy location
+    var rightEdge = 500;
+    if (this.x > rightEdge) {
+        this.x = -50;
+        this.setNewSpeed();
+    }
 
     // Handle collision with player
+    this.checkCollisions();
 };
 
 // Draw the enemy on the screen, required method for game
@@ -36,31 +39,121 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Reset enemies in screen
+Enemy.prototype.setNewSpeed = function() {
+    var randomSpeedMult = Math.random() * (5 - 1 + 1) + 1;
+    this.speed = randomSpeedMult * 100;
+};
+
+Enemy.prototype.checkCollisions = function() {
+    var yDiff = this.y - player.y;
+    var xDiff = this.x - player.x;
+
+    if (yDiff > -20 && yDiff < 20)
+    { 
+        if (xDiff > -50 && xDiff < 50)
+            player.die();
+    }
+};
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var player = function() {
-    this.sprite = "char-princess-girl.png";
+var Player = function(x, y) {
+    this.sprite = 'images/char-cat-girl.png';
+    this.x = x;
+    this.y = y;
+    this.score = 0;
+    this.displayScore();
 };
 
-player.prototype.update = function(dt) {
-
-    // do something
-};
-
-player.prototype.render = function() {
+Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-player.prototype.handleInput = function(keyCodes) {
-    // do something
+Player.prototype.update = function(dt) {
+    // make sure game is moving at same speed
+};
+
+Player.prototype.handleInput = function(key) {
+    // move player based on keyboard input
+    var leftbound = -2;
+    var rightbound = 402;
+    var top = 68;
+    var bottom = 400;
+
+    if (key === 'left')
+    {
+        if (this.x == leftbound)
+            this.x = this.x;
+        else
+            this.x -= 101;
+    }
+    if (key === 'right')
+    {
+        if (this.x == rightbound)
+            this.x = this.x;
+        else
+            this.x += 101;
+    }
+    if (key === 'up')
+    {
+        if (this.y == top)
+            this.win();
+        else
+            this.y -= 83;
+    }
+    if (key === 'down')
+    {
+        if (this.y == bottom)
+            this.y = this.y;
+        else
+            this.y += 83;
+    }
+};
+
+// Reset player location
+Player.prototype.resetLocation = function() {
+    this.x = 200;
+    this.y = 400;
+};
+
+// Increase score and reset player when player wins
+Player.prototype.win = function() {
+    this.score++;
+    this.resetLocation();
+    this.displayScore();
+};
+
+Player.prototype.die = function() {
+    this.score--;
+    this.resetLocation();
+    this.displayScore();
+};
+
+Player.prototype.displayScore = function() {
+    var scoreString = "Score: " + this.score.toString();
+    // var scoreText = document.getElement
+    document.querySelector("#score").innerHTML = scoreString;
+    // ctx.strokeText(scoreString, 400, 10);
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [];
+var enemy;
+for (var i = 0; i < 3; i++) {
+    var x = 0;
+    var y = 60;
+    var speed = Math.floor(Math.random() * (500 - 100 + 1)) + 100;
+    enemy = new Enemy(x, y + 83 * i, speed);
+    allEnemies.push(enemy);
+}
+
+var player = new Player(200, 400);
 
 
 // This listens for key presses and sends the keys to your
--- frogger/js/engine.js
+++ frogger/js/engine.js
@@ -91,7 +91,7 @@ var Engine = (function(global) {
      * render methods.
      */
     function updateEntities(dt) {
-        var allEnemies = [];
+        // var allEnemies = [];
         allEnemies.forEach(function(enemy) {
             enemy.update(dt);
         });
@@ -173,7 +173,11 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png'
     ]);
     Resources.onReady(init);
 
